a7183b47077591838c20cfa29d6981ecebcb8eea
cheps367 <cheps365.samsung@gmail.com>
2025-08-19 21:31:36 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,46 +5,6 @@
 #include <Geode/utils/web.hpp>
 #include <Geode/utils/casts.hpp>
 
-#include <Geode/binding/FMODAudioEngine.hpp>
-#include "cocos-ext.h"
-USING_NS_CC_EXT;
-using namespace geode::prelude;
-class TheMapLayer : public cocos2d::CCLayer {
-public:
-	CREATE_FUNC(TheMapLayer);
-    virtual bool init() override {
-        if (!CCLayer::init()) return false;
-
-		// Handle the BGM
-		FMODAudioEngine::sharedEngine()->stopAllMusic(0);
-    	FMODAudioEngine::sharedEngine()->playMusic("secretLoop.mp3", true, 1, 0);
-
-		// Handle loading assets
-		auto bg = cocos2d::CCSprite::create("mapbg.png");
-		bg->setPosition({ 240, 160 }); // center of 480x320 scene
-
-		// Scrolling support
-		auto windowSize = CCDirector::sharedDirector()->getWinSize();
-		auto scrolLayer = CCLayer::create();
-		scrolLayer->addChild(bg, -1);
-		auto scroll = CCScrollView::create({windowSize.width, windowSize.height}, scrolLayer);
-		scroll->setDirection(kCCScrollViewDirectionBoth);  
-		scroll->setTouchEnabled(true);
-		scroll->setContentSize({ windowSize.width * 2, windowSize.height * 2 });
-		this->addChild(scroll);
-
-		// Handle loading assets that dont scroll and show above it.
-		auto label = cocos2d::CCLabelBMFont::create("The Map", "goldFont.fnt");
-        label->setPosition({ 275, 300 }); // center of 480x320 scene
-        this->addChild(label);
-		
-        // Initialize SomeNode
-        return true;
-    }
-};
-
-
-
 /**
  * Brings cocos2d and all Geode namespaces to the current scope.
  */
@@ -137,40 +97,34 @@ class $modify(MyMenuLayer, MenuLayer) {
 	 * return type `void` and taking a `CCObject*`.
 	*/
 	void onMyButton(CCObject*) {
-		web::openLinkInBrowser("https://gdps.dimisaio.be/");
+		web::openLinkInBrowser("https://www.example.com/");
 	}
-
-	void onMoreGames(CCObject*) {
-		web::openLinkInBrowser("https://gdps.dimisaio.be/moregames.html");
-	} 
 };
 
+// Example for the Creator Layer
 #include <Geode/modify/CreatorLayer.hpp>
-#include <Geode/utils/web.hpp>
 class $modify(MyCreatorLayer, CreatorLayer) {
     bool init() override {
         if (!CreatorLayer::init()) return false;
 
+		// Make sure to reference all the buttons so the game knows what we are talking about.
         auto menu = static_cast<CCMenu*>(this->getChildByID("creator-buttons-menu"));
         if (!menu) return true;
 
+		// Removing the Versus Mode button as a demonstration of what is possible.
         auto mapBtn = static_cast<CCMenuItemSpriteExtra*>(menu->getChildByID("versus-button"));
         if (mapBtn) {
             mapBtn->setVisible(false);
         }
         return true;
     }
+	// The Map button. Comes from riredll but it was kept due to me wanting to show off FL Alert Layers
 	void onAdventureMap(CCObject*) {
-		// web::openLinkInBrowser("https://gdps.dimisaio.be/moregames.html");
-		auto myLayer = TheMapLayer::create();
-		
-		auto scene = cocos2d::CCScene::create();
-    	scene->addChild(myLayer);
-		
-        scene->addChild(myLayer);
-		
-		auto transition = cocos2d::CCTransitionFade::create(0.5f, scene); // fade over 0.5 seconds
-		cocos2d::CCDirector::sharedDirector()->replaceScene(transition);
+		FLAlertLayer::create(
+    	"FL Alert Layer Title",    // title
+    	"Hello, world!",  // content
+    	"wowie"        // button
+		)->show();
 	} 
 };
 
